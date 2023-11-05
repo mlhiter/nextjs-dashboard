743e533aaef7fb90b6efa1e4f585e3628ca460da
Alex Lee <3076438032@qq.com>
2023-11-05 08:55:01 +0800

    feat: suspense function

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,8 @@
-import { generateYAxis } from '@/app/lib/utils';
-import { CalendarIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+import { generateYAxis } from '@/app/lib/utils'
+import { CalendarIcon } from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/ui/fonts'
+import { Revenue } from '@/app/lib/definitions'
+import { fetchRevenue } from '@/app/lib/data'
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,18 +10,15 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
-  const chartHeight = 350;
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue()
+  const chartHeight = 350
   // NOTE: comment in this code when you get to this point in the course
 
-  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+  const { yAxisLabels, topLabel } = generateYAxis(revenue)
 
   if (!revenue || revenue.length === 0) {
-    return <p className="mt-4 text-gray-400">No data available.</p>;
+    return <p className="mt-4 text-gray-400">No data available.</p>
   }
 
   return (
@@ -34,8 +32,7 @@ export default async function RevenueChart({
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
-          >
+            style={{ height: `${chartHeight}px` }}>
             {yAxisLabels.map((label) => (
               <p key={label}>{label}</p>
             ))}
@@ -47,8 +44,7 @@ export default async function RevenueChart({
                 className="w-full rounded-md bg-blue-300"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
-              ></div>
+                }}></div>
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                 {month.month}
               </p>
@@ -61,5 +57,5 @@ export default async function RevenueChart({
         </div>
       </div>
     </div>
-  );
+  )
 }
