1d5afef18340ae98cab58f5d0065cc3386214768
Alex Lee <3076438032@qq.com>
2023-11-05 23:11:00 +0800

    feat: handing errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -19,18 +19,22 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
 
-  // 写入数据库
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
-  // 重新验证发票页面是否改变
-  revalidatePath('/dashboard/invoices')
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    }
+  }
 
-  // 重定向回发票页面
+  revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
 
@@ -45,16 +49,25 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' }
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`
-  revalidatePath('/dashboard/invoices')
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`
+    revalidatePath('/dashboard/invoices')
+    return { message: 'Deleted Invoice.' }
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
 }
